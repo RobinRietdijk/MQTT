8d5b08182945be4fc4cd665ae491397daaabc196
Robin Rietdijk <robinrietdijk@kabelfoon.net>
2024-02-15 02:47:29 +0100

    Add better error handling in Broker class

--- apps/mqtt_backend/Broker.ts
+++ apps/mqtt_backend/Broker.ts
@@ -2,57 +2,67 @@ import Aedes, { AedesOptions } from "aedes";
 import { createServer, ServerFactoryOptions } from "aedes-server-factory";
 
 export class Broker {
-  private port: number;
-  private aedesOptions: AedesOptions;
-  private serverOptions: ServerFactoryOptions;
-  private aedes?: Aedes;
-  private broker?: ReturnType<typeof createServer>;
+    private port: number;
+    private aedesOptions: AedesOptions;
+    private serverOptions: ServerFactoryOptions;
+    private aedes?: Aedes;
+    private broker?: ReturnType<typeof createServer>;
 
-  constructor(
-    port: number,
-    aedesOptions: AedesOptions = {},
-    serverOptions: ServerFactoryOptions = {},
-  ) {
-    this.port = port;
-    this.aedesOptions = aedesOptions;
-    this.serverOptions = serverOptions;
-  }
-
-  public updateOptions(
-    aedesOptions?: AedesOptions,
-    serverOptions?: ServerFactoryOptions,
-  ): void {
-    throw new Error("Not yet implemented");
-  }
-
-  public start(cb: (error?: Error) => void): void {
-    this.aedes = new Aedes(this.aedesOptions);
-    this.broker = createServer(this.aedes.handle, this.serverOptions);
-    this.broker
-      .listen(this.port, () => {
-        console.log(`Broker running on port ${this.port}`);
-        cb();
-      })
-      .on("error", (err) => {
-        console.log(`Failed to start broker on port ${this.port}:`, err);
-        cb(err);
-      });
-  }
-
-  public async close(cb: (error?: Error) => void): Promise<void> {
-    if (!this.broker || !this.aedes) {
-      cb(new Error("Server is not running"));
-      return;
+    constructor(
+        port: number,
+        aedesOptions: AedesOptions = {},
+        serverOptions: ServerFactoryOptions = {},
+    ) {
+        this.port = port;
+        this.aedesOptions = aedesOptions;
+        this.serverOptions = serverOptions;
     }
-    const broker = this.broker;
-    await new Promise<void>((resolve, reject) =>
-      broker.close((err) => {
-        if (err) reject(err);
-        else resolve();
-      }),
-    );
-    this.aedes.close();
-    this.broker = undefined;
-    this.aedes = undefined;
-  }
+
+    public updateOptions(
+        aedesOptions?: AedesOptions,
+        serverOptions?: ServerFactoryOptions,
+    ): void {
+        throw new Error("Not yet implemented");
+    }
+
+    public async start(): Promise<void> {
+        this.aedes = new Aedes();
+        this.broker = createServer(this.aedes, this.serverOptions);
+
+        await new Promise<void>((resolve, reject) => {
+            this.broker!.listen(this.port, () => {
+                console.log(`Broker running on port ${this.port}`);
+                resolve();
+            }).on("error", async (err) => {
+                console.log(`Failed to start broker on port ${this.port}:`, err.message);
+                await this.close().then(() => reject(err));
+            });
+        });
+    }
+
+    public async close(): Promise<void> {
+        if (!this.broker && !this.aedes) {
+            console.log('Broker or Aedes instances are already closed or were never initialized');
+            return;
+        }
+
+        if (this.broker) {
+            if (this.broker.listening) {
+                await new Promise<void>((resolve, reject) => {
+                    this.broker!.close((err) => {
+                        if (err) reject(err);
+                        else resolve();
+                    });
+                });
+            }
+            this.broker = undefined;
+        }
+
+        if (this.aedes) {
+            await new Promise<void>((resolve, reject) => {
+                this.aedes!.close(() => resolve());
+            });
+            this.aedes = undefined;
+        }
+    }
 }
